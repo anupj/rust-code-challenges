cf295a13521de57185af3c9734d3fb7cfe0e92c4
Anup Jadhav <anup.jadhav@gmail.com>
2022-10-04 12:22:23 +0100

    unique items exercise done 🎉

--- unique-items/src/lib.rs
+++ unique-items/src/lib.rs
@@ -1,35 +1,73 @@
-use std::collections::HashSet;
-
+#![allow(dead_code)]
 // Filters duplicates within a list.
 // Accepts a Vec<i32> reference and returns a
 // Vec<i32> with no duplicates.
 pub fn unique(list: &Vec<i32>) -> Vec<i32> {
-    let mut unique_set = HashSet::new();
-    // inserting values into a set
-    // will ensure that its unique
-    for i in list {
-        unique_set.insert(*i);
+    // if empty return empty vector
+    if list.len() == 0 {
+        return vec![];
     }
-    // convert the set back into a list
-    // let return_list: Vec<_> = unique_set.into_iter().collect();
-    let return_list: Vec<_> = Vec::from_iter(unique_set);
+    // clone into a result list
+    let mut result_list = list.clone();
+    // sort it
+    result_list.sort();
+    // dedupe it
+    result_list.dedup();
+    result_list
+}
 
-    return return_list;
+// Generic version of the above function
+fn generic_unique<T: Ord + Clone>(list: &Vec<T>) -> Vec<T> {
+    // if empty return empty vector
+    if list.len() == 0 {
+        return vec![] as Vec<T>;
+    }
+    let mut result_list = list.clone();
+    result_list.sort();
+    result_list.dedup();
+    result_list
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
-    fn test_with_no_duplicates() {
-        let list = vec![1, 4, 5];
+    fn test_empty_list() {
+        let list = vec![];
         assert_eq!(list, unique(&list));
+        let list2: Vec<String> = vec![];
+        assert_eq!(vec![] as Vec<String>, generic_unique(&list2));
     }
 
     #[test]
-    fn test_with_duplicates() {
+    fn test_sorted_with_no_duplicates() {
+        let list = vec![1, 4, 5];
+        assert_eq!(list, unique(&list));
+        let list2: Vec<&str> = vec!["abc", "def", "hij"];
+        assert_eq!(list2, generic_unique(&list2));
+    }
+
+    #[test]
+    fn test_sorted_list_with_duplicates() {
         let list = vec![1, 1, 3];
         assert_eq!(vec![1, 3], unique(&list));
+        let list2: Vec<&str> = vec!["abc", "abc", "def", "hij"];
+        assert_eq!(vec!["abc", "def", "hij"], generic_unique(&list2));
+    }
+
+    #[test]
+    fn test_unsorted_with_no_duplicates() {
+        let list = vec![4, 1, 5];
+        assert_eq!(vec![1, 4, 5], unique(&list));
+        let list2: Vec<&str> = vec!["abc", "hij", "def"];
+        assert_eq!(vec!["abc", "def", "hij"], generic_unique(&list2));
+    }
+
+    #[test]
+    fn test_unsorted_list_with_duplicates() {
+        let list = vec![3, 3, 1];
+        assert_eq!(vec![1, 3], unique(&list));
+        let list2: Vec<&str> = vec!["abc", "hij", "def", "def"];
+        assert_eq!(vec!["abc", "def", "hij"], generic_unique(&list2));
     }
 }
