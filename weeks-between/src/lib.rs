4d4da658b04f3a1e43f5f3dc18e133d3b1c30b4a
Anup Jadhav <anup.jadhav@gmail.com>
2022-10-05 19:32:30 +0100

    weeks between in progress

---
+++ weeks-between/src/lib.rs
@@ -0,0 +1,37 @@
+use chrono::DateTime;
+/// Assume that date1 and date2 are
+/// always valid dates with the following
+/// format YYYY-MM-DD
+/// When date2 occurs before date1 return a
+/// a negative number
+pub fn weeks_between(date1: &str, date2: &str) -> i32 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn same_day() {
+        let n_weeks = weeks_between("1010-10-10", "1010-10-10");
+        assert_eq!(n_weeks, 0);
+    }
+
+    #[test]
+    fn one_week() {
+        let n_weeks = weeks_between("1010-10-10", "1010-10-18");
+        assert_eq!(n_weeks, 1);
+    }
+
+    #[test]
+    fn past() {
+        let n_weeks = weeks_between("1010-10-18", "1010-10-10");
+        assert_eq!(n_weeks, -1);
+    }
+    // #[test]
+    // fn it_works() {
+    //     let result = add(2, 2);
+    //     assert_eq!(result, 4);
+    // }
+}
