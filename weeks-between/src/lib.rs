2ab8aa1f69ca08cbb4d4d087275bf3ce53e1c93f
Anup Jadhav <anup.jadhav@gmail.com>
2022-10-05 21:13:55 +0100

    weeks between finished 📆

--- weeks-between/src/lib.rs
+++ weeks-between/src/lib.rs
@@ -1,11 +1,15 @@
-use chrono::DateTime;
+use chrono::NaiveDate;
 /// Assume that date1 and date2 are
 /// always valid dates with the following
 /// format YYYY-MM-DD
 /// When date2 occurs before date1 return a
 /// a negative number
 pub fn weeks_between(date1: &str, date2: &str) -> i32 {
-    todo!()
+    let t1 = NaiveDate::parse_from_str(date1, "%Y-%m-%d").unwrap();
+    let t2 = NaiveDate::parse_from_str(date2, "%Y-%m-%d").unwrap();
+
+    let n_weeks = (t2 - t1).num_days() / 7;
+    n_weeks as i32
 }
 
 #[cfg(test)]
@@ -29,9 +33,4 @@ mod tests {
         let n_weeks = weeks_between("1010-10-18", "1010-10-10");
         assert_eq!(n_weeks, -1);
     }
-    // #[test]
-    // fn it_works() {
-    //     let result = add(2, 2);
-    //     assert_eq!(result, 4);
-    // }
 }
