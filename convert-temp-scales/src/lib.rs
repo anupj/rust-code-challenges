b857604f7d82a37a849a74a8beb4a90f1c292339
Anup Jadhav <anup.jadhav@gmail.com>
2022-10-05 12:15:04 +0100

    convert celsius to fahrenheit and vice versa 🌡

--- convert-temp-scales/src/lib.rs
+++ convert-temp-scales/src/lib.rs
@@ -12,6 +12,19 @@ enum Scale {
 }
 
 impl Temperature {
+    fn new_celsius(degrees: f32) -> Self {
+        Temperature {
+            degrees: degrees,
+            scale: Scale::Celsius,
+        }
+    }
+
+    fn new_fahrenheit(degrees: f32) -> Self {
+        Temperature {
+            degrees: degrees,
+            scale: Scale::Fahrenheit,
+        }
+    }
     fn to_celsius(&self) -> f32 {
         match self.scale {
             Scale::Fahrenheit => {
@@ -45,7 +58,7 @@ mod tests {
             degrees: 90.0,
             scale: Scale::Fahrenheit,
         };
-        assert_eq!(32.2, temp_in_fh.to_celsius());
+        assert_eq!("32.22", format!("{:.2}", temp_in_fh.to_celsius()));
         assert_eq!(90.0, temp_in_fh.to_fahrenheit());
     }
 
@@ -57,5 +70,30 @@ mod tests {
         };
         assert_eq!(40.0, temp_in_fh.to_celsius());
         assert_eq!(104.0, temp_in_fh.to_fahrenheit());
+    }
+
+    #[test]
+    fn one_degree() {
+        let cold = Temperature::new_celsius(1.0);
+        assert!((cold.to_fahrenheit() - 33.8) < 0.01);
+        assert!((cold.to_fahrenheit() - 33.8) >= 0.0);
+    }
+
+    #[test]
+    fn boiling() {
+        let hot = Temperature::new_celsius(100.0);
+        assert!((hot.to_fahrenheit() - 212.0) < 0.01);
+        assert!((hot.to_fahrenheit() - 212.0) >= 0.0);
+    }
+
+    #[test]
+    fn freezing() {
+        let freezing = Temperature {
+            degrees: Temperature::new_celsius(0.0).to_fahrenheit(),
+            scale: Scale::Fahrenheit,
+        };
+
+        assert!(freezing.to_celsius() < 0.001);
+        assert!(freezing.to_celsius() > -0.01);
     }
 }
