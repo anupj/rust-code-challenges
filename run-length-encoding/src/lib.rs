1563dee6eadeca329ca4b0083eb8d9ee378abfad
Anup Jadhav <anup.jadhav@gmail.com>
2022-10-06 20:40:36 +0100

    run length encoding in progress ⚠️

--- run-length-encoding/src/lib.rs
+++ run-length-encoding/src/lib.rs
@@ -1,5 +1,35 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/// Encode  and decode string of characters
+/// e.g. LinkedIn => 1L1i1k1e1d1I1n
+/// e.g. AAAAAaAA => 5A1a2A
+/// Extra credit
+///  - long run & arbitrary byte streams
+
+pub fn run_length_encode(input: &str) -> String {
+    let mut encoded_str = String::new();
+    if input.is_empty() {
+        return encoded_str;
+    }
+    let mut char_count = 1u32;
+    let mut prev_char = input.chars().next().unwrap();
+
+    for (i, c) in input.chars().enumerate() {
+        if i == 0 {
+            println!("i is {}", i);
+            continue;
+        }
+        if c == prev_char {
+            char_count += 1;
+        } else {
+            encoded_str.push_str(&char_count.to_string());
+            encoded_str.push_str(&prev_char.to_string());
+            prev_char = c;
+            char_count = 1;
+        }
+    }
+    encoded_str.push_str(&char_count.to_string());
+    encoded_str.push_str(&prev_char.to_string());
+
+    encoded_str
 }
 
 #[cfg(test)]
@@ -7,8 +37,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_non_repeating_string() {
+        assert_eq!(
+            "1L1i1n1k1e1d1I1n".to_string(),
+            run_length_encode("LinkedIn")
+        );
+    }
+
+    #[test]
+    fn test_repeating_string() {
+        assert_eq!("5A1a2A".to_string(), run_length_encode("AAAAAaAA"));
     }
 }
